e78ddf9cb9f09779d2716771f7a1371d72b7d88a
Steffen Tiedemann Christensen <steffen@refresh.dk>
2009-07-27 21:05:04 +0000

    Update code for Jyske Bank: Cleanup with _doc() function, and include an account_name() function

--- banklet.js
+++ banklet.js
@@ -3,11 +3,9 @@
 // javascript:void(function(){var%20s=document.createElement('script');s.src='http://refresh.dk/bank/banklet.js';document.getElementsByTagName('head')[0].appendChild(s);}())
 
 // Insert jQuery
-if (!$) {
     var s = document.createElement('script');
     s.src='http://code.jquery.com/jquery-latest.js';
     document.getElementsByTagName('head')[0].appendChild(s);
-}
 
 try {
     // Testing stuff
@@ -17,15 +15,28 @@ try {
             // Bank: Jyske Bank, http://www.jyskenetbank.dk
             // Maintainer: steffen@refresh.dk
             bank_name: 'Jyske Bank',
-            test_bank: function(){return($('#main').length && $('#main').get(0).contentDocument && ('#main').get(0).contentDocument.documentElement && new RegExp('JyskeNetbank').test($('frameset').get(0).innerHTML));},
-            test_page: function(){return($($('#main')[0].contentDocument.documentElement).find('table.DataTable tr td').length>0);},
+            _doc: function(){if($('#main').length && $('#main').get(0).contentDocument) return($('#main').get(0).contentDocument); else return(null);},
+            test_bank: function(){var d=this._doc(); return(d && new RegExp('JyskeNetbank').test($('frameset').get(0).innerHTML));},
+            test_page: function(){var d=this._doc(); return($(d).find('table.DataTable tr td').length>0);},
+            account_name: function(){
+                var d = this._doc(); 
+                var account_name = '';
+                $($(d).find('select').get(0).options).each(function(){
+                        if(this.selected) account_name = this.innerHTML.substring(14);
+                    });
+                return(account_name);
+            },
             transactions: function(){
-                var items = $($('#main').get(0).contentDocument.documentElement).find('table.DataTable tbody tr');
+                var d=this._doc();
+                var items = $(d).find('table.DataTable tbody tr');
                 var tx = [];
                 $.each(items, function(){
                         var values = $(this).find('td');
                         if (values.size()==7) {
-                            var date = $(values.get(2)).text().trim(); // Date as 2009-07-21
+                            var _date = $(values.get(2)).text().trim();
+                            if (_date.length==0) return;
+                            var arr_date = _date.match(/(\d{2})\.(\d{2})\.(\d{4})/);
+                            var date = arr_date[3] + '-' + arr_date[2] + '-' + arr_date[1];  // Date as 2009-07-21
                             if (date.length==0) return;
                             var text = $(values.get(3)).text().trim(); // text
                             var change = $(values.get(5)).text().trim().replace(/\./img, '').replace(/,/img, '.'); // float number
@@ -58,10 +69,10 @@ try {
                 $.each(items, function(){
                         var values = $(this).find('td');
                         if (values.size()==8) {
-                            var _date = $(values.get(1)).text().trim(); // Date as 2009-07-21
+                            var _date = $(values.get(1)).text().trim();
                             if (_date.length==0) return;
                             var arr_date = _date.match(/(\d{2})-(\d{2})-(\d{4})/);
-                            var date = arr_date[3] + '-' + arr_date[2] + '-' + arr_date[1];
+                            var date = arr_date[3] + '-' + arr_date[2] + '-' + arr_date[1]; // Date as 2009-07-21
                             var text = $(values.get(2)).text().replace(/\t/img, '').trim(); // text
                             var change = $(values.get(4)).text().trim().replace(/[^0-9\-,\.]/img, '').replace(/\./img, '').replace(/,/img, '.'); // float number
                             var total = $(values.get(6)).text().trim().replace(/[^0-9\-,\.]/img, '').replace(/\./img, '').replace(/,/img, '.'); // float number
@@ -93,6 +104,6 @@ try {
     } else {
         console.debug('Nothing known about this bank or webpage.');
     }
-} catch(e) {alert(e);}
+} catch(e) {console.debug(e);}
 
 
