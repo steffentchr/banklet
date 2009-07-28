0c8b1af5049ea03c7724904ec4d717a4d5b592bd
Steffen Tiedemann Christensen <steffen@refresh.dk>
2009-07-28 16:52:37 +0000

    Post data to central service; check data validity

--- banklet.js
+++ banklet.js
@@ -15,6 +15,7 @@ try {
             // Bank: Jyske Bank, http://www.jyskenetbank.dk
             // Maintainer: steffen@refresh.dk
             bank_name: function(){return('Jyske Bank');},
+            currency: function(){return('DKK');},
             _doc: function(){if($('#main').length && $('#main').get(0).contentDocument) return($('#main').get(0).contentDocument); else return(null);},
             test_bank: function(){var d=this._doc(); return(d && new RegExp('JyskeNetbank').test($('frameset').get(0).innerHTML));},
             test_page: function(){var d=this._doc(); return($(d).find('table.DataTable tr td').length>0);},
@@ -39,6 +40,7 @@ try {
                             var text = $(values.get(3)).text().trim(); // text
                             var amount = $(values.get(5)).text().trim().replace(/\./img, '').replace(/,/img, '.'); // float number
                             var total = $(values.get(6)).text().trim().replace(/\./img, '').replace(/,/img, '.'); // float number
+                            if (date.length==0 || text.length==0 || amount.length==0 || total.length==0) return;
                             tx.push({date:date, text:text, amount:amount, total:total});
                         }
                     });
@@ -49,6 +51,7 @@ try {
             // Bank: Nordea, http://www.netbank.nordea.dk
             // Maintainer: steffen@refresh.dk
             bank_name: function(){return('Nordea');},
+            currency: function(){return('DKK');},
             _doc:function(){var ret; $('frame').each(function(){if ($(this).attr('name').indexOf('content')==0) ret=this.contentDocument;}); return(ret);},
             test_bank: function(){var d = this._doc(); return(d && $(d).find('body.ContentBGcolor').length);},
             test_page: function(){var d = this._doc(); return(d && $(d).find('body.ContentBGcolor table.LeftmenuFaneQuickFix tr.tableBody').length);},
@@ -72,6 +75,7 @@ try {
                             var text = $(values.get(2)).text().replace(/\t/img, '').trim(); // text
                             var amount = $(values.get(4)).text().trim().replace(/[^0-9\-,\.]/img, '').replace(/\./img, '').replace(/,/img, '.'); // float number
                             var total = $(values.get(6)).text().trim().replace(/[^0-9\-,\.]/img, '').replace(/\./img, '').replace(/,/img, '.'); // float number
+                            if (date.length==0 || text.length==0 || amount.length==0 || total.length==0) return;
                             tx.push({date:date, text:text, amount:amount, total:total});
                         }
                     });
@@ -82,6 +86,7 @@ try {
             // Bank: BEC (Eik Bank, ....), http://www.bec.dk
             // Maintainer: steffen@refresh.dk
             bank_name: function(){return('BEC');},
+            currency: function(){return('DKK');},
             test_bank: function(){
                 if ($("frame[name='main']").length) {
                     // BEC is tricky since the user's bank is embedding the web bank service in a frame from a different service domain.
@@ -121,6 +126,7 @@ try {
                             var text = $(values.get(2)).text().replace(/\t/img, '').trim(); // text
                             var amount = $(values.get(4)).text().trim().replace(/[^0-9\-,\.]/img, '').replace(/\./img, '').replace(/,/img, '.'); // float number
                             var total = $(values.get(5)).text().trim().replace(/[^0-9\-,\.]/img, '').replace(/\./img, '').replace(/,/img, '.'); // float number
+                            if (date.length==0 || text.length==0 || amount.length==0 || total.length==0) return;
                             tx.push({date:date, text:text, amount:amount, total:total});
                         }
                     });
@@ -137,20 +143,72 @@ try {
                 match = true;
                 // Right bank
                 if(this.test_page()) {
-                    data = {account_name:this.account_name(), bank_name:this.bank_name(), transactions:this.transactions()};
+                    data = {currency:this.currency(), account_name:this.account_name(), bank_name:this.bank_name(), transactions:this.transactions()};
                     success = true;
-                    console.debug(data);
+                    //console.debug(data);
                 } else {
-                    console.debug('Not right page for ' + this.bank_name());
+                    alert('Not right page for ' + this.bank_name());
                 }
                 return(false);
             }
         });
     if (match) {
-        if (success) {}
+        if (success) {
+            var f = document.createElement('form');
+            f.setAttribute('action', 'http://bank.labs.refresh.dk/bank/import');
+            f.setAttribute('method', 'post');
+            f.setAttribute('target', '_new');
+
+            var account_name_input = document.createElement('input');
+            account_name_input.setAttribute('type', 'hidden');
+            account_name_input.setAttribute('name', 'account_name');
+            account_name_input.setAttribute('value', data.account_name);
+            f.appendChild(account_name_input);
+
+            var currency_input = document.createElement('input');
+            currency_input.setAttribute('type', 'hidden');
+            currency_input.setAttribute('name', 'currency');
+            currency_input.setAttribute('value', data.currency);
+            f.appendChild(currency_input);
+
+            var bank_name_input = document.createElement('input');
+            bank_name_input.setAttribute('type', 'hidden');
+            bank_name_input.setAttribute('name', 'bank_name');
+            bank_name_input.setAttribute('value', data.bank_name);
+            f.appendChild(bank_name_input);
+
+            $.each(data.transactions, function(i,tx){
+                    var date_input = document.createElement('input');
+                    date_input.setAttribute('type', 'hidden');
+                    date_input.setAttribute('name', 'date'+i);
+                    date_input.setAttribute('value', tx.date);
+                    f.appendChild(date_input);
+
+                    var text_input = document.createElement('input');
+                    text_input.setAttribute('type', 'hidden');
+                    text_input.setAttribute('name', 'text'+i);
+                    text_input.setAttribute('value', tx.text);
+                    f.appendChild(text_input);
+
+                    var amount_input = document.createElement('input');
+                    amount_input.setAttribute('type', 'hidden');
+                    amount_input.setAttribute('name', 'amount'+i);
+                    amount_input.setAttribute('value', tx.amount);
+                    f.appendChild(amount_input);
+
+                    var total_input = document.createElement('input');
+                    total_input.setAttribute('type', 'hidden');
+                    total_input.setAttribute('name', 'total'+i);
+                    total_input.setAttribute('value', tx.total);
+                    f.appendChild(total_input);
+                });
+
+            document.body.appendChild(f);
+            f.submit();
+        }
     } else {
-        console.debug('Nothing known about this bank or webpage.');
+        alert('Nothing known about this bank or webpage.');
     }
-} catch(e) {console.debug(e);}
+} catch(e) {alert(e);}
 
 
